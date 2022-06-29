081ce7ca2ffa0ab757ed4f8e7ef0f4e98c4f5cb7
Aman Raj <aman.raj@unisys.com>
2022-06-29 19:01:54 +0530

    useContext

--- src/CompC.js
+++ src/CompC.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ContextExample } from "./App";
 const CompC = () => {
+  //we can also access the context using useContext hook
+  const contextValue1 = useContext(ContextExample);
   return (
     <>
       <ContextExample.Consumer>
@@ -8,7 +10,9 @@ const CompC = () => {
           return (
             <div>
               This is a double nested Component C (Context Example)
-              contextValue: {contextValue}
+              <br /> contextValue: {contextValue}
+              <br />
+              conntextValue1: {contextValue1}
             </div>
           );
         }}
