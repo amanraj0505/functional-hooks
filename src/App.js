c13936a01c22c937b8adfe58a4f165ee2fd27b3f
Aman Raj <aman.raj@unisys.com>
2023-01-15 20:43:44 +0530

    UseRefHook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./cat.png";
 import "./App.css";
-import { useEffect, useState, createContext } from "react";
+import { useEffect, useState, createContext, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { counterActions } from "./redux/actions/counterActions";
 import CompA from "./CompA";
@@ -10,7 +10,9 @@ function App() {
   const [title, setTitle] = useState("");
   const reduxCount = useSelector((state) => state.counter.counter);
   const dispatch = useDispatch();
-  // const store = useStore();
+  const inputRef = useRef();
+  const refValue = useRef("");
+  const [inputText, setInputText] = useState("");
   useEffect(() => {
     console.log("useEffect called (Counter)");
     fetch("https://jsonplaceholder.typicode.com/todos/" + String(counter))
@@ -22,18 +24,38 @@ function App() {
   });
   useEffect(() => {
     console.log("useEffect called at start (componentDidMount)");
+    inputRef.current.focus();
   }, []);
   useEffect(() => {
     return () => {
       console.log("cleanup(componentWillUnmount)");
     };
   }, []);
+  //here current is persisted and we are updating it manually but this refvalue.current can be use to access the previous value of the state.
+  // if we use useSte here trying to persist the previous value we can't do that as the useSate value will be updated after reRender;
+  /**
+   * Assignments to the 'previousValue' variable from inside React Hook useEffect will be lost after each render. To preserve the value over time,
+   * store it in a useRef Hook and keep the mutable value in the '.current' property. Otherwise, you can move this variable directly inside useEffect (trying to use var)
+   */
+  useEffect(() => {
+    refValue.current = inputText;
+  }, [inputText]);
   return (
     <>
       <ContextExample.Provider value={"Aman"}>
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
+            <p>UseRef Usage</p>
+            <input
+              ref={inputRef}
+              onChange={(e) => setInputText(e.target.value)}
+            />
+            <p>
+              Input text used to be{" "}
+              <b style={{ color: "red" }}>{refValue.current}</b> but now its{" "}
+              <b style={{ color: "red" }}>{inputText}</b>
+            </p>
             <p>
               {counter}
               <br></br>
